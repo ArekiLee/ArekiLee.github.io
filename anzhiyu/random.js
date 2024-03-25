72aa44aad5effbec20321a70da613ba95cedd37f
ArekiLee <areki.lee@qq.com>
2024-03-25 20:37:53 +0800

    Site updated: 2024-03-25 20:37:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/86c75619.html","posts/177574ba.html","posts/12da5a1b.html","posts/35779054.html","posts/2930152c.html","posts/ef0aaf19.html","posts/5fc129b0.html","posts/6c6b907f.html","posts/d25cdecd.html"];function toRandomPost(){
+var posts=["posts/86c75619.html","posts/177574ba.html","posts/35779054.html","posts/ef0aaf19.html","posts/12da5a1b.html","posts/6c6b907f.html","posts/5fc129b0.html","posts/d25cdecd.html","posts/2930152c.html","posts/6c7d87d6.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
